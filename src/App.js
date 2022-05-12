bc4106b2fe2aa1807dbe7be4ea0bd8ffe6f8a955
nadezdatsygankova <nadiapritchina@gamil.com>
2022-05-12 14:29:37 -0400

    feat: add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 
 import './App.css';
+import './scss/app.scss'
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
     <div className="App">
-       <h1> hi</h1>
+      <div class="wrapper">
+        <Header />
+        <div class="content">
+          <div class="container">
+            <div class="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 class="content__title">All Pizzas</h2>
+            <div class="content__items">
+              <PizzaBlock title="ULTIMATE PEPPERONI" price={11.69} />
+              <PizzaBlock title="PEPPERONI" price={11.69} />
+              <PizzaBlock title="CHARCUTERIE" price={17.49} />
+              <PizzaBlock title="HOT HONEY" price={16.29} />
+              <PizzaBlock title="BUFFALO CHICKEN" price={17.89} />
+              <PizzaBlock title="BUTTER CHICKEN" price={16.89} />
+              <PizzaBlock title="PROSCIUTTO & ARUGULA" price={14.29} />
+              <PizzaBlock title="HAWAIIAN" price={13.29} />
+              <PizzaBlock title="MEAT SUPREME" price={16.49} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meat Pizzas</li>
+        <li>Veggie Pizzas</li>
+        <li>Specials</li>
+        <li>The Bread Basket</li>
+        <li>Drinks</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
