c7ff8debc699eca6e331058f37e5a6ef7675acb4
nadezdatsygankova <nadiapritchina@gamil.com>
2022-05-14 19:51:13 -0400

    feat: sort featured

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,16 @@
+import React from 'react';
+
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+
+  const list = ['Featured', 'Lowest Price', 'Highest Price'];
+  const sortName = list[selected];
+  const onClickListItem = (i) => {
+    setSelected(i);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -14,18 +26,24 @@ function Sort() {
           />
         </svg>
         <b>Sort by:</b>
-        <span>Featured</span>
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">Featured</li>
-          <li>Lowest Price</li>
-          <li>Highest Price</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {list.map((name, i) => (
+              <li
+                key={i}
+                onClick={() => onClickListItem(i)}
+                className={selected === 'i' ? 'active' : ''}>
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
-
 
 export default Sort;
